cc600bfa1e9b2ea7388db548e3c92eefacf15bf4
WoodenScout <leonardotoffalini@gmail.com>
2023-10-20 07:58:22 +0200

    code fix

--- board.py
+++ board.py
@@ -36,7 +36,7 @@ class Board:
         for tile in self.tiles_list:
             tile.draw(display)
         
-        if DEBUG == 1 and self.selected_piece is not None: print(f'position of selected piece: {self.selected_piece.pos}')
+        if DEBUG >= 1 and self.selected_piece is not None: print(f'position of selected piece: {self.selected_piece.pos}')
 
 
     def get_tile_from_pos(self, x, y):
@@ -55,14 +55,10 @@ class Board:
         if DEBUG >= 2: print(f'clicked tile: {clicked_tile}')
         if DEBUG >= 2: print(f'selected piece: {self.selected_piece}')
         if DEBUG >= 2: print(f'clicked tile piece: {clicked_tile.piece}')
-        if clicked_tile.piece is not None:
-            temp = Color.BLACK if clicked_tile.piece.color == Color.BLACK else Color.RED
-        else:
-            temp = 'none'
-        if DEBUG >= 2: print(f'clicked tile piece color: {temp}')
+        if DEBUG >= 2: print(f'clicked tile piece color: {clicked_tile.piece}')
         if DEBUG >= 2: print(f'turn: {self.turn}')
 
-        if self.selected_piece is None and clicked_tile.piece is not None and temp == self.turn:
+        if self.selected_piece is None and clicked_tile.piece is not None and clicked_tile.piece == self.turn:
             self.selected_piece = clicked_tile.piece
 
         elif self.selected_piece is not None and self.selected_piece.move(clicked_tile):
