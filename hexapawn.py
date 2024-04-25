8713c38754bcdfe8051408d95d13eadb4908e3e3
WoodenScout <leonardotoffalini@gmail.com>
2024-04-25 11:59:55 +0200

    adds note to outdated file

--- hexapawn.py
+++ hexapawn.py
@@ -1,28 +1,27 @@
+"""
+This file is outdate and not used anywhere.
+"""
+
 import copy
 from time import time
 
-class Board():
-    def __init__(self, board:bool=None, turn:int=1):
+
+class Board:
+    def __init__(self, board: bool = None, turn: int = 1):
         if board == None:
-            self.board = [
-                [-1, -1, -1],
-                [0,  0,  0],
-                [1,  1,  1]
-            ]
+            self.board = [[-1, -1, -1], [0, 0, 0], [1, 1, 1]]
         else:
             self.board = board
 
         self.turn = turn
 
-
-    def _convert(self, x:int) -> str:
+    def _convert(self, x: int) -> str:
         if x == 1:
             return "X"
         elif x == -1:
             return "O"
         elif x == 0:
             return " "
-
 
     def _hash_pos(self):
         result = ""
@@ -31,30 +30,45 @@ class Board():
                 result += self._convert(self.board[i][j])
         return result
 
-
     def displayBoard(self) -> None:
-        print(f'{self._convert(self.board[0][0])}|{self._convert(self.board[0][1])}|{self._convert(self.board[0][2])}')
-        print('------')
-        print(f'{self._convert(self.board[1][0])}|{self._convert(self.board[1][1])}|{self._convert(self.board[1][2])}')
-        print('------')
-        print(f'{self._convert(self.board[2][0])}|{self._convert(self.board[2][1])}|{self._convert(self.board[2][2])}')
+        print(
+            f"{self._convert(self.board[0][0])}|{self._convert(self.board[0][1])}|{self._convert(self.board[0][2])}"
+        )
+        print("------")
+        print(
+            f"{self._convert(self.board[1][0])}|{self._convert(self.board[1][1])}|{self._convert(self.board[1][2])}"
+        )
+        print("------")
+        print(
+            f"{self._convert(self.board[2][0])}|{self._convert(self.board[2][1])}|{self._convert(self.board[2][2])}"
+        )
 
-
-    def getMoves(self, row:int, col:int) -> list:
+    def getMoves(self, row: int, col: int) -> list:
         moves = []
         # preliminary check
         if self.board[row][col] == self.turn:
-            if row-self.turn in range(3) and col in range(3) and self.board[row-self.turn][col] == 0:
-                moves.append((row-self.turn, col))
-            if row-self.turn in range(3) and col-1 in range(3) and self.board[row-self.turn][col-1] == -self.turn:
-                moves.append((row-self.turn, col-1))
-            if row-self.turn in range(3) and col+1 in range(3) and self.board[row-self.turn][col+1] == -self.turn:
-                moves.append((row-self.turn, col+1))
+            if (
+                row - self.turn in range(3)
+                and col in range(3)
+                and self.board[row - self.turn][col] == 0
+            ):
+                moves.append((row - self.turn, col))
+            if (
+                row - self.turn in range(3)
+                and col - 1 in range(3)
+                and self.board[row - self.turn][col - 1] == -self.turn
+            ):
+                moves.append((row - self.turn, col - 1))
+            if (
+                row - self.turn in range(3)
+                and col + 1 in range(3)
+                and self.board[row - self.turn][col + 1] == -self.turn
+            ):
+                moves.append((row - self.turn, col + 1))
 
         return moves
 
-
-    def move(self, row:int, col:int, newRow:int, newCol:int):
+    def move(self, row: int, col: int, newRow: int, newCol: int):
         moves = dict()
         for i in range(3):
             for j in range(3):
@@ -67,7 +81,6 @@ class Board():
         else:
             print("Invalid move")
 
-
     def checkWinner(self) -> int:
         for j in range(3):
             if self.board[0][j] == 1:
@@ -78,7 +91,7 @@ class Board():
         moves = dict()
         for i in range(3):
             for j in range(3):
-                if (move:=self.getMoves(i, j)):
+                if move := self.getMoves(i, j):
                     moves[(i, j)] = move
 
         if len(moves) == 0:
@@ -86,47 +99,45 @@ class Board():
 
         return 0
 
-
-    def minimax(self, depth:int, player:int) -> float:
+    def minimax(self, depth: int, player: int) -> float:
         moves = dict()
         for i in range(3):
             for j in range(3):
-                if (move := self.getMoves(i, j)):
+                if move := self.getMoves(i, j):
                     moves[(i, j)] = move
 
         if player == 1:
-            value = -float('inf')
+            value = -float("inf")
             for key in moves:
                 for val in moves[key]:
                     child = Board(copy.deepcopy(self.board), self.turn)
                     child.move(key[0], key[1], val[0], val[1])
-                    score = child.minimax(depth-1, -player)
+                    score = child.minimax(depth - 1, -player)
                     value = max(value, score)
             return value
 
         else:
-            value = float('inf')
+            value = float("inf")
             for key in moves:
                 for val in moves[key]:
                     child = Board(copy.deepcopy(self.board), self.turn)
                     child.move(key[0], key[1], val[0], val[1])
-                    score = child.minimax(depth-1, -player)
+                    score = child.minimax(depth - 1, -player)
                     value = min(value, score)
             return value
 
-
-    def alphabeta(self, depth:int, alpha:float, beta:float, player:int) -> float:
+    def alphabeta(self, depth: int, alpha: float, beta: float, player: int) -> float:
         look_up_table = dict()
         print(look_up_table)
 
         moves = dict()
         for i in range(3):
             for j in range(3):
-                if (move := self.getMoves(i, j)):
+                if move := self.getMoves(i, j):
                     moves[(i, j)] = move
 
         if player == 1:
-            value = -float('inf')
+            value = -float("inf")
             for key in moves:
                 for val in moves[key]:
                     child = Board(copy.deepcopy(self.board), self.turn)
@@ -135,7 +146,7 @@ class Board():
                     if (hashed_pos := child._hash_pos()) in look_up_table:
                         score = look_up_table[hashed_pos]
                     else:
-                        score = child.alphabeta(depth-1, alpha, beta, -player)
+                        score = child.alphabeta(depth - 1, alpha, beta, -player)
                         look_up_table[hashed_pos] = score
                     value = max(value, score)
                     if value > beta:
@@ -144,7 +155,7 @@ class Board():
             return value
 
         else:
-            value = float('inf')
+            value = float("inf")
             for key in moves:
                 for val in moves[key]:
                     child = Board(copy.deepcopy(self.board), self.turn)
@@ -153,7 +164,7 @@ class Board():
                     if (hashed_pos := child._hash_pos()) in look_up_table:
                         score = look_up_table[hashed_pos]
                     else:
-                        score = child.alphabeta(depth-1, alpha, beta, -player)
+                        score = child.alphabeta(depth - 1, alpha, beta, -player)
                         look_up_table[hashed_pos] = score
                     value = min(value, score)
                     if value < alpha:
@@ -161,18 +172,17 @@ class Board():
                     beta = min(beta, value)
             return value
 
-
-    def get_best_move(self, depth:int, player:int, ab:bool=False):
+    def get_best_move(self, depth: int, player: int, ab: bool = False):
         moves = dict()
         for i in range(3):
             for j in range(3):
-                if (move := self.getMoves(i, j)):
+                if move := self.getMoves(i, j):
                     moves[(i, j)] = move
 
         if player == 1:
-            value = -float('inf')
+            value = -float("inf")
         elif player == -1:
-            value = float('inf')
+            value = float("inf")
 
         best_move = -1
 
@@ -181,7 +191,7 @@ class Board():
                 child = Board(copy.deepcopy(self.board), self.turn)
                 child.move(key[0], key[1], val[0], val[1])
                 if ab:
-                    score = child.alphabeta(depth, -float('inf'), float('inf'), -player)
+                    score = child.alphabeta(depth, -float("inf"), float("inf"), -player)
                 else:
                     score = child.minimax(depth, -player)
                 curr_move = (key[0], key[1], val[0], val[1])
@@ -190,9 +200,8 @@ class Board():
                     value, best_move = score, curr_move
                 elif player == -1 and score < value:
                     value, best_move = score, curr_move
-        
-        return "you already lost" if best_move == -1 else best_move
 
+        return "you already lost" if best_move == -1 else best_move
 
 
 def main():
@@ -223,5 +232,5 @@ def main():
             break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
